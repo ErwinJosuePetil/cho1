f0cca60bffc8a1c1b24921579caa8c214153b180
sam won <erwinpetil1996@yahoo.com>
2025-02-09 10:03:51 +0800

    records

--- admin/datatables/patient-record.php
+++ admin/datatables/patient-record.php
@@ -2,6 +2,7 @@
 // Include the database connection
 require_once 'db.php';
 require_once 'functions.php';
+include 'modals/update-patient.php';
 
 // Fetch the mothers' data
 $mothers = get_mothers();
@@ -58,40 +59,8 @@ $mothers = get_mothers();
     </tfoot>
 </table>
 
-<!-- Modal -->
-<div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="updateModalLabel">Update Patient Record</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="update_record.php" method="POST">
-                    <input type="hidden" name="id" id="patient-id">
-                    <div class="form-group">
-                        <label for="admission_date">Admission Date</label>
-                        <input type="date" class="form-control" name="admission_date" id="admission_date" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="discharge_date">Discharge Date</label>
-                        <input type="date" class="form-control" name="discharge_date" id="discharge_date">
-                    </div>
-                    <div class="form-group">
-                        <label for="complications">Complications</label>
-                        <textarea class="form-control" name="complications" id="complications"></textarea>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Save changes</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+
+
 
 <script>
     $('#updateModal').on('show.bs.modal', function (event) {
---
+++ admin/modals/update-patient.php
@@ -0,0 +1,34 @@
+<!-- Modal -->
+<div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="updateModalLabel">Update Patient Record</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="update_record.php" method="POST">
+                    <input type="hidden" name="id" id="patient-id">
+                    <div class="form-group">
+                        <label for="admission_date">Admission Date</label>
+                        <input type="date" class="form-control" name="admission_date" id="admission_date" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="discharge_date">Discharge Date</label>
+                        <input type="date" class="form-control" name="discharge_date" id="discharge_date">
+                    </div>
+                    <div class="form-group">
+                        <label for="complications">Complications</label>
+                        <textarea class="form-control" name="complications" id="complications"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
