f5f081c4b12d3e964300399399abb7ae330162b4
sam won <erwinpetil1996@yahoo.com>
2025-03-14 08:54:39 +0800

    nav

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -16,159 +16,93 @@
             <li class="nav-item active">
                 <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Dashboard</span>
+                </a>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
 
             <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-            <!-- Nav Item - Charts -->
+            <div class="sidebar-heading">Patient Management</div>
+            
             <li class="nav-item">
                 <a class="nav-link" href="patient.php">
                     <i class="fas fa-fw fa-users"></i>
-                    <span>All Patients</span></a>
+                    <span>All Patients</span>
+                </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="records.php">
                     <i class="fas fa-fw fa-folder-open"></i>
-                    <span>Patient Records</span></a>
-                    
+                    <span>Patient Records</span>
+                </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="pregnancy.php">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pregnancy records</span></a>
+                    <i class="fas fa-fw fa-baby"></i>
+                    <span>Pregnancy Records</span>
+                </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="medical.php">
-                <i class="fa fa-user-md" aria-hidden="true"></i>
-                    <span>Medical History</span></a>
+                    <i class="fas fa-fw fa-user-md"></i>
+                    <span>Medical History</span>
+                </a>
             </li>
-
             <li class="nav-item">
                 <a class="nav-link" href="labaratory.php">
-                <i class="fa fa-flask" aria-hidden="true"></i>
-                    <span>Lab & Diagnostic Tests</span></a>
+                    <i class="fas fa-fw fa-flask"></i>
+                    <span>Lab & Diagnostic Tests</span>
+                </a>
             </li>
 
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="charts.php">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-
-            Nav Item - Tables -->
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="tables.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li> -->
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <!-- <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.php">Buttons</a>
-                        <a class="collapse-item" href="cards.php">Cards</a>
-                    </div>
-                </div>
-            </li> -->
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <!-- <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.php">Colors</a>
-                        <a class="collapse-item" href="utilities-border.php">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.php">Animations</a>
-                        <a class="collapse-item" href="utilities-other.php">Other</a>
-                    </div>
-                </div>
-            </li> -->
-
-            <!-- Divider -->
+            <!-- New Sections -->
             <hr class="sidebar-divider">
+            <div class="sidebar-heading">Appointments & Services</div>
 
-            <!-- Heading -->
-            <!-- <div class="sidebar-heading">
-                Addons
-            </div> -->
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <!-- <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+            <li class="nav-item">
+                <a class="nav-link" href="appointments.php">
+                    <i class="fas fa-fw fa-calendar-check"></i>
+                    <span>Appointments</span>
                 </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6> -->
-                        <!-- <a class="collapse-item" href="login.php">Login</a> -->
-                        <!-- <a class="collapse-item" href="register.php">Register</a>
-                        <a class="collapse-item" href="forgot-password.php">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.php">404 Page</a>
-                        <a class="collapse-item" href="blank.php">Blank Page</a>
-                    </div>
-                </div>
-            </li> -->
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="immunization.php">
+                    <i class="fas fa-fw fa-syringe"></i>
+                    <span>Immunization Records</span>
+                </a>
+            </li>
 
+            <!-- System Management -->
+            <hr class="sidebar-divider">
+            <div class="sidebar-heading">System Management</div>
 
-
+            <li class="nav-item">
+                <a class="nav-link" href="staff.php">
+                    <i class="fas fa-fw fa-user-nurse"></i>
+                    <span>Staff Management</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="reports.php">
+                    <i class="fas fa-fw fa-chart-bar"></i>
+                    <span>Reports & Analytics</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="users.php">
+                    <i class="fas fa-fw fa-user-shield"></i>
+                    <span>User & Role Management</span>
+                </a>
+            </li>
+            
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Sidebar Toggler (Sidebar) -->
+            <!-- Sidebar Toggler -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
-
-
-
         </ul>
         <!-- End of Sidebar -->
-        <!-- Scroll to Top Button-->
-        <a class="scroll-to-top rounded" href="#page-top">
-            <i class="fas fa-angle-up"></i>
-        </a>
-
-        <!-- Logout Modal-->
-        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="logout.php">Logout</a>
-
-                    </div>
-                </div>
-            </div>
-        </div>
